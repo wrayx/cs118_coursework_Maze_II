9b088d357dc8fad1aec2fb948a6af9ecb6c15b7b
Wray Ge <ycg@Wrays-MBP.wifi-campus.warwick.ac.uk>
2019-10-15 13:55:05 +0100

    Exercise 2

--- DumboController.java
+++ DumboController.java
@@ -1,44 +1,66 @@
-/*
-* File: DumboController.java
-* Created: 17 September 2002, 00:34
-* Author: Stephen Jarvis
-*/
-
+/**
+ * File: DumboController.java
+ * Created: 17 September 2002, 00:34
+ * Author: Stephen Jarvis
+ */
 import uk.ac.warwick.dcs.maze.logic.IRobot;
 
 public class DumboController
 {
-
+	/**
+	 * The count varbiable +1 with each block the robot has
+	 * moved. when it hits the multiple of 8 it will guide 
+	 * the robot to choose a new random direction
+	 */ 
+	int random_cnt = 0;
 	public void controlRobot(IRobot robot) {
 
-		int randno;
-		int direction;
+		double randno;
+		/**
+		 * The default value of direction is AHEAD
+		 */
+		int direction = IRobot.AHEAD;
 		int walls = 0;
-		/* Since there isn't a default value for the switch 
-			cases down below, the string needed to be initiallized */
+		/**
+		 * 1 in random_prob times it will select a new 
+		 * random direction
+		 */
+		int random_prob = 8;
+		/**
+		 * Since there isn't a default value for the switch 
+		 * cases down below, the string needed to be initiallized
+		*/ 
 		String direction_output = "";
 		String circumstances;
 
-		/* do once to choose a random direction to turn to,
-			and then check if the place ahead of it is a wall.
-			if it is a wall, then it will repeat the loop 
-			until the condition is not true anymore */
-		do {
-			// Select a random number
-			randno = (int) Math.round(Math.random()*3);
+		random_cnt++;
+		/* if the robot look ahead and there is a wall then it will randomly choose 
+			a new directionto turn to 
+			Regardless of the above condition, if the cnt hits the multiple of 8. 
+			The robot will jump into this loop to choose a new random direction */
+		while (robot.look(direction) == IRobot.WALL | random_cnt % random_prob == 0) {
+			// Select a random number within the range of 0 and 3
+			randno = Math.random()*4;
 
 			// Convert this to a direction
-			if (randno == 0)
+			if (randno > 0 && randno < 1)
 				direction = IRobot.LEFT;
-			else if (randno == 1)
+			else if (randno > 1 && randno < 2)
 				direction = IRobot.RIGHT;
-			else if (randno == 2)
+			else if (randno > 2 && randno < 3)
 				direction = IRobot.BEHIND;
 			else
 				direction = IRobot.AHEAD;
-
-			robot.face(direction); /* Face the robot in this direction */
-		} while (robot.look(IRobot.AHEAD) == IRobot.WALL);
+			
+			/* cnt hits 8, robot has chosen a new direction. 
+				Increase the cnt once to get the robot out of this loop */
+			if(random_cnt % random_prob == 0)
+				random_cnt++;
+		}
+		// testing
+		// System.out.println(random_cnt);
+		// Face the robot in this direction
+		robot.face(direction); 
 
 		
 
@@ -80,6 +102,7 @@ public class DumboController
 
 		System.out.println("I'm going " + direction_output + " " + circumstances);
 
+		// testing
 		// System.out.println(walls);
 	}
 
