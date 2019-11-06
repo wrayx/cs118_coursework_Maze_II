20b3adbac5b1f830c9316b655a315d74973e52e9
Wray <yyeween@gmail.com>
2019-11-06 17:37:57 +0000

    Ex2 class seperation finished

--- Ex2.java
+++ Ex2.java
@@ -4,13 +4,11 @@ import uk.ac.warwick.dcs.maze.logic.IRobot;
 public class Ex2 {
     private static int cnt = 0;
     private static final int[] directions = { IRobot.AHEAD, IRobot.LEFT, IRobot.RIGHT, IRobot.BEHIND };
-    private Stack<Integer> headingUnexplored;
-    private Stack<Integer> arrivedHeading;
+    private RobotData robotData;
     public void controlRobot(IRobot robot) {
         int direction = 0;
         if (cnt == 0){
-            headingUnexplored = new Stack<Integer>();
-            arrivedHeading = new Stack<Integer>();
+            robotData = new RobotData();
         }
         int numOfExits = numOfExits(robot);
         if (numOfExits == 1)
@@ -25,8 +23,7 @@ public class Ex2 {
     }
 
     public void reset() {
-        headingUnexplored = new Stack<Integer>();
-        arrivedHeading = new Stack<Integer>();
+        robotData.resetRobotData();
     }
 
     private int numOfExits(IRobot robot) {
@@ -82,19 +79,19 @@ public class Ex2 {
         int[] exits = exitsCanGo(robot);
         int j = 0;
         if (passageExits(robot) == numOfExits(robot) - 1){
-            arrivedHeading.push(robot.getHeading());
+            robotData.addArrivedHeading(robot.getHeading());
             for (int exit : exits) {
                 // add to stack of the heading waiting to explore
                 if (exit != IRobot.BEHIND)
-                    headingUnexplored.push(exit);
+                    robotData.addUnexploredHeading(exit);
             }
         }
-        robot.setHeading(arrivedHeading.peek());
+        robot.setHeading(robotData.peekArrivedHeading());
         if (passageExits(robot) == 0) {
-            robot.setHeading(reverseAbsDirection(arrivedHeading.pop()));
+            robot.setHeading(reverseAbsDirection(robotData.popArrivedHeading()));
             return IRobot.AHEAD;
         }
-        return headingUnexplored.pop();
+        return robotData.popUnexploredHeading();
     }
 
     public int reverseAbsDirection(int absDirection) {
@@ -122,5 +119,44 @@ public class Ex2 {
                 exits[j++] = directions[i];
         }
         return exits;
+    }
+}
+
+class RobotData {
+    private Stack<Integer> headingUnexplored;
+    private Stack<Integer> arrivedHeading;
+
+    public RobotData() {
+        headingUnexplored = new Stack<Integer>();
+        arrivedHeading = new Stack<Integer>();
+    }
+
+    public void resetRobotData() {
+        headingUnexplored.clear();
+        arrivedHeading.clear();
+    }
+
+    public int popArrivedHeading() {
+        return arrivedHeading.pop();
+    }
+
+    public int peekArrivedHeading() {
+        return arrivedHeading.peek();
+    }
+
+    public int addArrivedHeading(int heading) {
+        return arrivedHeading.push(heading);
+    }
+
+    public int popUnexploredHeading() {
+        return headingUnexplored.pop();
+    }
+
+    public int peekUnexploredHeading() {
+        return headingUnexplored.peek();
+    }
+
+    public int addUnexploredHeading(int unexploredHeading) {
+        return headingUnexplored.push(unexploredHeading);
     }
 }
